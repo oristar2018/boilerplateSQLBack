c3aad124f25c6da62822d630bf3bceb7ecfe6ae1
Oristar2018 <ulricvanstatter@gmail.com>
2019-02-05 22:14:08 +0100

    basic bcrypt password hash for SQL model

--- routes/routes.js
+++ routes/routes.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const router = require('express-promise-router')();
-const db = require('../sequelize/config');
-const auththree = require('../sequelize/model');
-const createAccount = require('../createAccountClass');
+const express = require("express");
+const router = require("express-promise-router")();
+const db = require("../sequelize/config");
+const auththree = require("../sequelize/model");
+const createAccount = require("../createAccountClass");
 
-router.route('/').get((req, res, next) => {
+router.route("/").get((req, res, next) => {
 	var Docs;
-	auththree.findAll()
-		.then((docs) => {console.log(docs);
+	auththree
+		.findAll()
+		.then(docs => {
+			console.log(docs);
 
-		 Docs = docs;
-
+			Docs = docs;
 		})
 		.catch(err => console.log(err));
 
-		res.json(Docs)
-	
+	res.json(Docs);
 });
 
-router.route('/signup').post((req, res, next) => {
-
+router.route("/signup").post((req, res, next) => {
 	const accountObject = new createAccount(req.body.name)
-											.setFirstName(req.body.firstName)
-											.setUserId(req.body.userId)
-											.setPassword(req.body.password);
+		.setFirstName(req.body.firstName)
+		.setUserId(req.body.userId)
+		.setPassword(req.body.password);
 
-	auththree.create(accountObject).then((docs) => { res.redirect('/')})
-								   .catch(err => console.log(err))
-
+	auththree
+		.create(accountObject)
+		.then(docs => {
+			res.redirect("/");
+		})
+		.catch(err => console.log(err));
 });
-
-
 
 module.exports = router;
--- sequelize/model.js
+++ sequelize/model.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const db = require('./config');
+const bcrypt = require("bcryptjs");
 
 const auththree = db.define('auththree', {
 
@@ -25,5 +26,20 @@ const auththree = db.define('auththree', {
 
 
 }, {timestamps: false});
+
+
+auththree.beforeCreate(async function(user, options) {
+  //console.log(this.password);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(user.password, salt);
+    user.password = passwordHash;
+    return user.password
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
 
 module.exports = auththree;
